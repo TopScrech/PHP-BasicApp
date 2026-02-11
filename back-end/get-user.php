82f4b7b74b217321e0404f59ad1b6ebabc90a5a1
TopScrech <sergei_saliukov@icloud.com>
2026-02-11 22:01:47 +0100

    get-user

--- back-end/get-user.php
+++ back-end/get-user.php
@@ -2,4 +2,30 @@
 
 header('Content-Type: application/json');
 
+if(!isset($_GET['id'])) {
+    sendError('id missing', __LINE__);
+}
+
+if (!ctype_digit($_GET['id'])) {
+    sendError('id must be an int', __LINE__);
+}
+
+require_once(__DIR__.'/protected/database.php');
+
+try {
+    $query = $db->prepare('SELECT * FROM users WHERE id = :id LIMIT 1');
+    $query->bindValue(':id', $_GET['id']); // Anti SQL injection
+    $query->execute();
+    $row = $query->fetch();
+
+    if(!$row) {
+        sendError('id does not exist', __LINE__);
+    }
+
+    echo '{"status": 1, "data": '.json_encode($row).' }';
+    exit();
+} catch(PDOException $ex) {
+    sendError('error executing query', __LINE__);
+}
+
 echo '{"status": 0, "data": {"id": 1, "name": "Goida"}}';
