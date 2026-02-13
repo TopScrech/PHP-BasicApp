2366ec482e0080a0885d2bc89b739c44440ac307
TopScrech <sergei_saliukov@icloud.com>
2026-02-13 13:19:10 +0100

    displaying user names in a web page

---
+++ front-end/src/routes/User.svelte
@@ -0,0 +1,9 @@
+<script>
+    import {users} from './data-users.js'
+    export let user = {}
+</script>
+
+<div class="user">
+    <!--{user.name}-->
+    <input type="text" value={user.name}>
+</div>
---
+++ front-end/src/routes/Users.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {users} from './data-users.js'
+    import {onMount} from "svelte";
+    import User from './User.svelte'
+
+    onMount(async() => {
+        const url = 'http://localhost:8888/back-end/get-users.php'
+        let res = await fetch(url)
+        res = await res.json()
+        console.log(res)
+        let data = res.data
+        $users = data
+    })
+</script>
+
+<div id="users">
+    {#each $users as user}
+        <User {user}/>
+    {/each}
+</div>
