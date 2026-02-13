5bae9b360f2436d9f593517c6e53a58d4bf4fdf2
TopScrech <sergei_saliukov@icloud.com>
2026-02-13 10:29:55 +0100

    Changes - Added header('Access-Control-Allow-Origin: *'); to all routes - Imported Svelte

--- back-end/get-user.php
+++ back-end/get-user.php
@@ -1,5 +1,5 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 if(!isset($_GET['id'])) {
--- back-end/get-users.php
+++ back-end/get-users.php
@@ -1,5 +1,5 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 // The next line replaces itself with database.php
